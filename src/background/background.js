c34ddd14c9328115f1597a38382a987c07a4677c
Sam Willington <last_eyes@protonmail.com>
2024-12-02 17:33:46 +0900

    Last stretch, all things done

--- src/background/background.js
+++ src/background/background.js
@@ -14,14 +14,8 @@ if (browserAPI.webRequest && browserAPI.webRequest.onCompleted) {
         if (!challengeId) return;
 
         if (details.statusCode >= 200 && details.statusCode < 300) {
-          console.log('문제를 해결했습니다!');
-          console.log('Challenge ID:', challengeId);
-          
-          // Discord 웹훅 전송
           const message = await DMessage.buildSolvedMessage(challengeId);
           await DWebhook.sendToAll(message);
-        } else {
-          console.log('문제를 해결하지 못했습니다.');
         }
       }
     },
--- src/module/performance.js
+++ src/module/performance.js
@@ -15,7 +15,7 @@ class Performance {
         const solveRate = cnt_solvers / hits * 100;  // 풀이 성공률 (%)
         const ppMultiplier1 = 10 / solveRate; // 10%를 기본 PP로 적용
         const ppMultiplier2 = 2 / Math.log10(Math.max(100, 100 + cnt_solvers)); // 풀이자 수가 많을 수록 패널티 (100명을 기본으로 둠)
-        const ppMultiplier3 = 3 / Math.log10(Math.max(1000, 1000 + hits)); // 조회수가 많을 수록 보상 (100명을 기본으로 둠)
+        const ppMultiplier3 = 3 / Math.log10(Math.max(1000, 1000 + hits)); // 조회수가 많을 수록 패널티 (1000명을 기본으로 둠)
 
         const pp = basePP * ppMultiplier1 * ppMultiplier2 * ppMultiplier3;
 
